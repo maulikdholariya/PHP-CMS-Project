cff5246e27ca7079257d0cbc9a818f4b306a1b0b
Maulik <maulikdholariya14@gmail.com>
2020-08-03 20:38:38 +0530

    Add loader in admin

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -42,6 +42,9 @@ if (!isset($_SESSION['user_role'])) {
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <link href="css/styles.css" rel="stylesheet">
+
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <!-- <script type="text/javascript"> -->
 
