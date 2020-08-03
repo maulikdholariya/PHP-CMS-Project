6d3c07655b5c233be085fcbece590f2d0b5d7545
Maulik <maulikdholariya14@gmail.com>
2020-08-03 16:14:30 +0530

    sort post by author

--- index.php
+++ index.php
@@ -39,7 +39,9 @@ while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
                 <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
             </h2>
             <p class="lead">
-                by <a href="index.php"><?php echo $post_author; ?></a>
+
+            <!-- without p_id its work change to author_posts.php isset = author -->
+                by <a href="author_posts.php?author=<?php echo $post_author; ?>&p_id="><?php echo $post_author; ?></a>
             </p>
             <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date; ?></p>
             <hr>
