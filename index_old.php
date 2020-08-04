b1fb99f76158e341f10016859a7a6847552f0848
Maulik <maulikdholariya14@gmail.com>
2020-08-04 20:53:20 +0530

    Paggination add

--- index_old.php
+++ index_old.php
@@ -12,34 +12,7 @@
 
         <div class="col-md-8">
             <?php
-    $per_page =2;
-        if(isset($_GET['page'])){
-
-             $page = $_GET['page'];
-        }else{
-
-
-        $page = "";
-
-        }
-
-        if($page == "" || $page == 1){
-
-            $page_1 = 0;
-        }else{
-            
-            $page_1 = ($page * $per_page)-$per_page;
-        }
-
-
-
-$post_query_count = "SELECT * FROM posts";
-$find_count = mysqli_query($connection, $post_query_count);
-$count = mysqli_num_rows($find_count);
-
-$count = ceil($count / $per_page);
-
-$query = "SELECT * FROM posts LIMIT $page_1 ,$per_page";
+$query = "SELECT * FROM posts";
 $select_all_posts_query = mysqli_query($connection, $query);
 while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
     $post_id = $row['post_id'];
@@ -62,27 +35,25 @@ while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
             </h1>
 
             <!-- First Blog Post -->
-            <h1><?php echo $count; ?></h1>
             <h2>
                 <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
             </h2>
             <p class="lead">
 
-                <!-- without p_id its work change to author_posts.php isset = author -->
+            <!-- without p_id its work change to author_posts.php isset = author -->
                 by <a href="author_posts.php?author=<?php echo $post_author; ?>&p_id="><?php echo $post_author; ?></a>
             </p>
             <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date; ?></p>
             <hr>
 
             <a href="post.php?p_id=<?php echo $post_id; ?>">
-                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
+            <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
             </a>
 
 
             <hr>
             <p><?php echo $post_content; ?></p>
-            <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span
-                    class="glyphicon glyphicon-chevron-right"></span></a>
+            <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
             <hr>
 
@@ -102,35 +73,5 @@ while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
     <!-- /.row -->
 
     <hr>
-
-    <ul class="pager">
-
-        <?php 
-
-for($i =1; $i <=$count; $i++){
-
-    if($i == $page){
-
-        echo "<li><a class='active_link' href='index.php?page={$i}'>{$i}</a></li>";
-
-    }else{
-
-        echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";
-
-    }
-
-
-
-
-}
-
-
-
-?>
-
-
-
-    </ul>
-
 
     <?php include "includes/footer.php";?>
