6f4bc716da029a9bc2a133eeb39aff86b654a7b7
Maulik <maulikdholariya14@gmail.com>
2020-08-11 13:14:32 +0530

    active navigation link

--- includes/navigation.php
+++ includes/navigation.php
@@ -22,7 +22,24 @@ $select_all_categories_query = mysqli_query($connection, $query);
 while ($row = mysqli_fetch_assoc($select_all_categories_query)) {
     $cat_id = $row['cat_id'];
     $cat_title = $row['cat_title'];
-    echo "<li><a href='../cms001/category.php?category=$cat_id'> {$cat_title}</a></li>";
+
+    $category_class = '';
+    $registration_class = '';
+
+    $pageName = basename($_SERVER['PHP_SELF']);
+
+    $registration = 'registration.php';
+
+    if(isset($_GET['category']) && $_GET['category'] == $cat_id){
+
+        $category_class = 'active';
+    }elseif($pageName==$registration){
+
+        $registration_class = 'active';
+
+    }
+
+    echo "<li class='$category_class'><a href='../cms001/category.php?category=$cat_id'> {$cat_title}</a></li>";
 
 }
 
@@ -31,7 +48,7 @@ while ($row = mysqli_fetch_assoc($select_all_categories_query)) {
                 <li>
                     <a href="admin">Admin</a>
                 </li>
-                <li>
+                <li class='<?php echo $registration_class ; ?>'>
                     <a href="registration.php">Registration</a>
                 </li>
                 <li>
